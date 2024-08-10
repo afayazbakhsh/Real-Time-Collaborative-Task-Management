e7d07b23c748549cd77eb8ff8fa313e58f1bd971
amir fayazbakhsh <a.fayazbakhsh1@gmail.com>
2024-08-10 22:43:35 +0330

    fix register route

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Auth\UserRegisterController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post(UserRegisterController::class, '/register');
+Route::post('/register', UserRegisterController::class);
