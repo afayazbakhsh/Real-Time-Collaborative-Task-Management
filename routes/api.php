1d341648c3bfbf15d2f6d308f5862e57a4f3356c
amir fayazbakhsh <a.fayazbakhsh1@gmail.com>
2024-08-11 22:54:25 +0330

    add project rest api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\UserRegisterController;
+use App\Http\Controllers\Projects\CreateProjectController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::post('/register', UserRegisterController::class);
+
+Route::prefix('projects')->group(function () {
+    Route::post('/create', CreateProjectController::class);
+});
