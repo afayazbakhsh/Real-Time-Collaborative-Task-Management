6f2d97539e9acf3b2a840d0e440dfa0d402ae025
amir fayazbakhsh <a.fayazbakhsh1@gmail.com>
2024-10-09 22:33:51 +0330

    feat : add name to routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\Projects\CreateProjectController;
 use App\Http\Controllers\Projects\IndexProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('projects')->middleware('auth:api')->group(function () {
-    Route::get('/', IndexProjectController::class);
-    Route::post('/', CreateProjectController::class);
+Route::prefix('projects')
+    ->middleware('auth:api')
+    ->name('projects.')
+    ->group(function () {
+    Route::get('/', IndexProjectController::class)->name('index');
+    Route::post('/', CreateProjectController::class)->name('create');
 });
