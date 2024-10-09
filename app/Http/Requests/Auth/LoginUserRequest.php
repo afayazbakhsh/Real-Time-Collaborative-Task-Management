3bbfbc9a9d768d91cd75d230fb2cb3323ead476b
amir fayazbakhsh <a.fayazbakhsh1@gmail.com>
2024-10-09 21:50:32 +0330

    feat : extend from parent request class

--- app/Http/Requests/Auth/LoginUserRequest.php
+++ app/Http/Requests/Auth/LoginUserRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests\Auth;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\RequestAbstract;
 
-class LoginUserRequest extends FormRequest
+class LoginUserRequest extends RequestAbstract
 {
     public function authorize(): bool
     {
